e6f901d25d707cc99194d40a1f40eeabb8790847
Muhammed Yazeen <myazeen01@gmail.com>
2026-06-26 12:12:31 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,15 @@
+# capital H and I in pattern
+
+for i in range(7):
+    if i == 3:
+        print("********")
+    else:
+        print("*      *")
+print()
+
+for i in range(1,7):
+    if i == 1 or i == 6 :
+        print("********")
+    else:
+        print("   *")
+print()
